66c15dc36d251cb58a22202a321938d25618acf6
rizkijuliansyaham <rizki.juliansyah.a.m@gmail.com>
2023-09-18 20:02:48 +0700

    test foto & kotak

--- belajar-swift-ui/ContentView.swift
+++ belajar-swift-ui/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CustomLogoView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomLogoView : View {
+    var body: some View {
+        VStack{
+            Image("profile-pic").resizable().padding(20).frame(width: 200, height: 200).background(Color("warnaCustom")).cornerRadius(30)//.foregroundColor(Color.yellow)
+        }
     }
 }
